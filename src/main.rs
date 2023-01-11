89355ab8f5860848a5d4c66240d1723a67de1973
Phil Baum <philipp.baum@mailbox.org>
2023-01-11 20:40:52 +0100

    feat: optional give path to settings file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod clean_channel;
 mod settings;
 
+use std::env;
+
 use config::Config;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
@@ -14,8 +16,15 @@ async fn main() {
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
+    let mut settings_location = "settings.toml";
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        settings_location = &args[1];
+    }
+
     let settings = Config::builder()
-        .add_source(config::File::with_name("settings.toml"))
+        .add_source(config::File::with_name(settings_location))
         .build()
         .expect("Issue building configuration.")
         .try_deserialize()
